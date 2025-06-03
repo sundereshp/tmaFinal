885c85227064956a2d6f54634039de495d048c4a
sundereshp <sundereshp@gmail.com>
2025-06-03 12:27:22 +0530

    updated config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base:'/tmaFinal/frontend/dist/',
   server: {
     host: "::",
     port: 8080,
