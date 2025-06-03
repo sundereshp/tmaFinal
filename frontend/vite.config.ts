5fc9a9ca5d58494a6c5d05a31088a8aeccf3ee07
sundereshp <sundereshp@gmail.com>
2025-06-03 16:02:38 +0530

    changed to hashrouter

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:'/tmaFinal/frontend/dist/',
+  base: '',
   server: {
     host: "::",
     port: 8080,
@@ -16,5 +16,10 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    // outDir: 'dist',
+    // assetsDir: 'assets',
+    // sourcemap: true,
   },
 }));
